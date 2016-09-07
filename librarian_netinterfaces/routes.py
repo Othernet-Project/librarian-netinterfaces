967a15ddde219b51c040125c26746ef3565b9e0b
Andrean Franc <andrean.franc@gmail.com>
2016-09-07 13:37:35 +0200

    Drop message about device reboot since it's no longer happening

--- librarian_netinterfaces/routes.py
+++ librarian_netinterfaces/routes.py
@@ -42,8 +42,7 @@ class NetSettings(XHRPartialFormRoute):
     def form_valid(self):
         commands = self.config['wireless.restart_commands']
         exts.tasks.schedule(restart_services, args=(commands,), delay=5)
-        return dict(message=_('Network settings have been saved. The device'
-                              'is going to reboot now.'),
+        return dict(message=_('Network settings have been saved.'),
                     redirect_url=i18n_url('dashboard:main'))
 
     def form_invalid(self):
